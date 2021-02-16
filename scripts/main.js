f7ae58d8be8bfce63ebea373bd98250313016c8b
Sherzod <nadirovsherzod82@gmail.com>
2021-02-16 13:39:15 +0500

    r1

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+let ss = document.querySelector('.ss');
+let sa = document.querySelector('.navbar-toggler');
+
+
+ss.addEventListener('click', function () {
+    sa.classList.add('.collapsed')
+    
+})
